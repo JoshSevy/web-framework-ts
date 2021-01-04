25c9681bd5bb4d3af38f9e3e0b8712b00ce409c1
JoshSevy <joshuasevy@outlook.com>
2021-01-03 22:59:57 -0700

    Add get set and on methods to User model class

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const user = new User({ name: 'Josh', age: 38 });
 console.log(user.get('name'));
 console.log(user.get('age'));
 
-user.set({name: 'newname', age: 9999});
+user.set({name: 'newname'});
 
 console.log(user.get('name'));
 console.log(user.get('age'));
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,10 +1,14 @@
 
 interface UserProps {
-  name: string;
-  age: number;
+  name?: string;
+  age?: number;
 }
 
+type Callback = () => void;
+
 export class User {
+  events: { [key: string]: Callback[] } = {};
+
   constructor(private data: UserProps) {}
 
   get(propName: string): (number | string) {
@@ -13,5 +17,15 @@ export class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback) {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger() {
+
   }
 }
