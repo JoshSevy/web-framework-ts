14078dd1954bd440b4de8d163d37058c17fca34c
JoshSevy <joshuasevy@outlook.com>
2021-03-05 21:09:27 -0700

    Add fetch method to user class

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,7 @@
 import { Eventing } from "./Eventing";
 import { Sync } from "./Sync";
 import { Attributes } from "./Attributes";
+import { AxiosResponse } from "axios";
 
 export interface UserProps {
   name?: string;
@@ -31,4 +32,18 @@ export class User {
     return this.events.trigger;
   }
 
+  set(update: UserProps): void {
+    this.attributes.set(update);
+    this.events.trigger('change');
+  }
+
+  fetch(): void {
+    const id = this.attributes.get('id');
+    if (typeof id !== 'number') {
+      throw new Error('Cannot fetch without an id');
+    }
+    this.sync.fetch(id).then((response: AxiosResponse): void => {
+      this.set(response.data);
+    })
+  }
 }
