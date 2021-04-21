3a355d714eed58219d27041abd763c810a6e44e0
JoshSevy <joshuasevy@outlook.com>
2021-04-20 19:53:16 -0600

    Add models

--- src/models/Model.ts
+++ src/models/Model.ts
@@ -27,13 +27,11 @@ export class Model<T extends HasId> {
     private sync: Sync<T>
   ) {}
 
-  get on() {
-    return this.events.on;
-  }
+  get = this.attributes.get;
 
-  get trigger() {
-    return this.events.trigger;
-  }
+  on = this.events.on;
+
+  trigger = this.events.trigger;
 
   set(update: T): void {
     this.attributes.set(update);
